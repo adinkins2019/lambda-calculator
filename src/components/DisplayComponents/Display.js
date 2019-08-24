0704b85c6fd5a8d4b9966896660897e7574f280a
adinkins2019 <afdinki@gmail.com>
2019-08-24 13:06:56 -0400

    Modified project files

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { tsPropertySignature } from "@babel/types";
 
-const Display = () => {
-  return <div>{/* Display any props data here */}</div>;
+const Display = (props) => {
+  return <div>{/* Display any props data here */
+    props.text
+  }</div>;
 };
+
+export default Display
